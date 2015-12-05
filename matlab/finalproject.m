2690a49b3311609c076fbe5a5a42174af218a722
ungheele <ungheele@gmail.com>
2015-12-05 15:48:58 -0500

    gg
    gg

--- matlab/finalproject.m
+++ matlab/finalproject.m
@@ -1,3 +1,5 @@
+% clear all
+
 %helix
 layers = 7;
 t = 0:pi/200:pi;
@@ -5,15 +7,16 @@ t = 0:pi/200:pi;
 X = sin(t);
 Y = cos(t);
 
+%% extract data from pointcloud
 pointClouds=[];
 indices=[];
 pointCloudsRow=[];
 pointCloudsRaw=load('eight_objects.pcd');
 
-numberOfPoints=length(pointCloudsRaw);
+numberOfPointsRaw=length(pointCloudsRaw);
 
-
-for i=1:numberOfPoints
+numberOfPoints
+for i=1:numberOfPointsRaw
     
     if isfinite(pointCloudsRaw(i,1))==1 && isfinite(pointCloudsRaw(i,2))==1 && isfinite(pointCloudsRaw(i,3))==1
         indices=[indices;i];
@@ -22,6 +25,33 @@ for i=1:numberOfPoints
         pointClouds=[pointClouds;pointCloudsRow];
     end
 end
+
+numberOfPoints=length(pointClouds)
+
+%% sampling
+
+
+numberOfNeighborCenter=100
+
+[sampledPointClouds,idx]=datasample(pointClouds,numberOfNeighborCenter);
+idx=idx'
+sampledPointClouds=[sampledPointClouds idx];
+neighbor=struct();
+neighborPoints=[];
+for i=1:length(idx)
+    for j=1:numberOfPoints
+        if sqrt((sampledPointClouds(i,1)-pointClouds(j,1))^2+(sampledPointClouds(i,2)-pointClouds(j,2))^2+(sampledPointClouds(i,3)-pointClouds(j,3))^2) < 0.01            
+            l=['neighbor' num2str(sampledPointClouds(i,4))];
+            neighborPoints=[neighborPoints;pointClouds(j,:)];
+            neighbor.(l)=neighborPoints;
+        end
+    end
+    neighborPoints=[];
+end
+
+%Accesing each neighborhood
+neighbor.(['neighbor' num2str(idx(2))])(2)
+
 
 numberOfPoints = layers * length(t);
 pointClouds = zeros(numberOfPoints, 3);
